36512cb3e073bd0f358d462da6f0a8f2c13dca85
Chirayu Desai <chirayudesai1@gmail.com>
2025-01-08 04:26:24 +0530

    Update fingerprint/build description from AP4A.250105.002
    Change-Id: I742bda13ef607bbbe55716c28a91b08ee21858d2

--- calyx_komodo.mk
+++ calyx_komodo.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := Pixel 9 Pro XL
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc="komodo-user 15 AP4A.241205.013 12621605 release-keys" \
-    BuildFingerprint=google/komodo/komodo:15/AP4A.241205.013/12621605:user/release-keys \
+    BuildDesc="komodo-user 15 AP4A.250105.002 12701944 release-keys" \
+    BuildFingerprint=google/komodo/komodo:15/AP4A.250105.002/12701944:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
