d7d5e7a03f6fe9ef16f1e39c1801f0c25200db64
Michael Bestas <mkbestas@gmail.com>
2025-07-30 19:25:30 +0300

    caimito: Drop camera extensions
    On Android 16, Pixel 9 series switched from PixelCameraServices.apk to
    PixelCameraServicesConnectivityClient.apk which has broader permissions
    including internet access, so we had commented it out in the proprietary
    files lists. Having the remaining blobs without the apk causes 3rd party
    camera apps to freeze for ~10 seconds when opening the app or switching
    between cameras, so remove the camera extensions altogether.
    
    Change-Id: I5512bc414426253960fde4a72eb9ea7b14a415ad

--- device-calyx.mk
+++ device-calyx.mk
@@ -31,10 +31,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Iwlan
 
-# Pixel Camera Services / Camera extensions
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/permissions/permissions_com.google.android.apps.camera.services.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.apps.camera.services.xml
-
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
