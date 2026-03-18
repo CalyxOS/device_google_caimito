84eba9f32950fda56424114c836363a88cfad2ad
Michael Bestas <mkbestas@gmail.com>
2026-03-18 03:09:47 +0200

    Initial CalyxOS makefiles
    Change-Id: I0aeb85302967c3ce004f7f18a967813684783088

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,6 +7,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_caiman.mk \
     $(LOCAL_DIR)/aosp_komodo.mk \
     $(LOCAL_DIR)/aosp_tokay.mk \
+    $(LOCAL_DIR)/calyx_caiman.mk \
+    $(LOCAL_DIR)/calyx_komodo.mk \
+    $(LOCAL_DIR)/calyx_tokay.mk \
     $(LOCAL_DIR)/lineage_caiman.mk \
     $(LOCAL_DIR)/lineage_komodo.mk \
     $(LOCAL_DIR)/lineage_tokay.mk
