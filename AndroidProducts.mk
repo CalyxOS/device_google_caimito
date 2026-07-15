CalyxOS/device_google_raviole

Initial CalyxOS makefiles
Change-Id: I0aeb85302967c3ce004f7f18a967813684783088

AndroidProducts.mk
@@ -6,5 +6,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_oriole.mk \
     $(LOCAL_DIR)/aosp_raven.mk \
+    $(LOCAL_DIR)/calyx_oriole.mk \
+    $(LOCAL_DIR)/calyx_raven.mk \
     $(LOCAL_DIR)/lineage_oriole.mk \
     $(LOCAL_DIR)/lineage_raven.mk

CalyxOS/device_google_caimito

AndroidProducts.mk
@@ -7,6 +7,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_caiman.mk \
     $(LOCAL_DIR)/aosp_komodo.mk \
     $(LOCAL_DIR)/aosp_tokay.mk \
+    $(LOCAL_DIR)/calyx_caiman.mk \
+    $(LOCAL_DIR)/calyx_komodo.mk \
+    $(LOCAL_DIR)/calyx_tokay.mk \
     $(LOCAL_DIR)/lineage_caiman.mk \
     $(LOCAL_DIR)/lineage_komodo.mk \
     $(LOCAL_DIR)/lineage_tokay.mk
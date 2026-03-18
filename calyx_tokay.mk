cfae29a6ac640100636814f9a32a9dec0ff608df
Michael Bestas <mkbestas@gmail.com>
2026-03-18 03:09:47 +0200

    Initial CalyxOS makefiles
    Change-Id: I0aeb85302967c3ce004f7f18a967813684783088

---
+++ calyx_tokay.mk
@@ -0,0 +1,28 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common stuff
+$(call inherit-product, vendor/calyx/config/common_phone.mk)
+
+# Inherit device configuration
+DEVICE_CODENAME := tokay
+DEVICE_PATH := device/google/caimito
+VENDOR_PATH := vendor/google/tokay
+$(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 9
+PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="tokay-user 16 BP4A.260205.002 14624737 release-keys" \
+    BuildFingerprint=google/tokay/tokay:16/BP4A.260205.002/14624737:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
